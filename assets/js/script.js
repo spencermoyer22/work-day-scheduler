d8be96052ef91e359e894586a8cdc5b38f6b6c82
Big Head <spencer.moyer16@yahoo.com>
2021-03-07 22:02:49 -0800

    added function to load tasks to screen from localStorage and added save icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $("#currentDay").text(currentDay);
 // save textarea values to array in localStorage
 
 $(".saveBtn").on("click", function() {
+    // set variables to set for localStorage array
     var taskDescription = $(this)
         .siblings("textarea")
         .val();
@@ -17,25 +18,39 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(taskId, taskDescription);
 });
 
+// load tasks to page from local storage
+$("textarea").each(function() {
+    // declare variable based on each textareas ID
+    var elementId = $(this).attr("id");
+
+    // grab items from array based on key selector(id)
+    $(this).val(localStorage.getItem(elementId));
+});
+
+// write a function to change background color of textareas based on time of day
 var changeColor = function () {
+    // declare current time in form of hours(military time)
     var time = parseInt(moment().format("H"));
 
+    // begin loop to compare each textareas id to current hour time
     $("textarea").each( function() {
         var taskHour = parseInt($(this).attr("id"));
-
-        if (time === taskHour) {
-            $(this).addClass("present");
+        if (time > taskHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
         }
         else if (time < taskHour) {
-            $(this).addClass("past");
-        }
-        else if (time > taskHour) {
             $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+        else {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
         }
     })
 };
 
 changeColor();
-
-
-$("#9").val(localStorage.getItem("9"));
